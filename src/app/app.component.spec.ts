0bb8ce8904371692f7988641942abc5181978829
alidorboz <dorbozali51@gmail.com>
2023-10-18 13:37:03 +0100

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'socialmediatest'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('socialmediatest');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('socialmediatest app is running!');
+  });
+});
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,38 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fa-solid fa-house',
+        label:'Dashboard'
+    },
+    {
+        routeLink: 'products',
+        icon: 'fa-solid fa-box-open',
+        label:'Products'
+    },
+    {
+        routeLink: 'statistics',
+        icon: 'fa-solid fa-chart-bar',
+        label:'Statistics'
+    },
+    {
+        routeLink: 'coupens',
+        icon: 'fa-solid fa-tags',
+        label:'Coupens'
+    },
+    {
+        routeLink: 'pages',
+        icon: 'fa-solid fa-file',
+        label:'Pages'
+    },
+    {
+        routeLink: 'media',
+        icon: 'fa-solid fa-camera',
+        label:'Media'
+    },
+    {
+        routeLink: 'settings',
+        icon: 'fa-solid fa-cog',
+        label:'Settings'
+    }
+
+];
